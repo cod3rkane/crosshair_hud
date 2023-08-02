286bf5238b305f0c7fcd409d6e4dbf887fa7e3e4
CoderKane <lopes.coder@gmail.com>
2023-08-02 00:35:38 -0400

    using sdl

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,102 @@
 extern crate gl;
-extern crate glfw;
 extern crate image;
+extern crate sdl2;
+extern crate winapi;
 
 mod engine;
 
 use std::ffi::c_void;
 
-use glfw::{Action, Context, Key};
+use sdl2::event::Event;
+use sdl2::hint::Hint;
+use sdl2::keyboard::Keycode;
+use sdl2::sys::{
+    SDL_GetWindowWMInfo, SDL_SetHint, SDL_WindowFlags,
+    SDL_HINT_WINDOW_FRAME_USABLE_WHILE_CURSOR_HIDDEN,
+};
+use sdl2::video::WindowPos;
 
 const WINDOW_WIDTH: u32 = 40;
 const WINDOW_HEIGHT: u32 = 40;
 
+fn find_sdl_gl_driver() -> Option<u32> {
+    for (index, item) in sdl2::render::drivers().enumerate() {
+        if item.name == "opengl" {
+            return Some(index as u32);
+        }
+    }
+    None
+}
+
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
 
     // OpenGL Settings
-    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
-        glfw::OpenGlProfileHint::Core,
-    ));
-    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 6);
+
+    // sdl2::hint::set(na);
+    // SDL_SetHint(SDL_HINT_WINDOW_FRAME_USABLE_WHILE_CURSOR_HIDDEN, false);
+    sdl2::hint::set("SDL_HINT_WINDOW_FRAME_USABLE_WHILE_CURSOR_HIDDEN", "0");
+    sdl2::hint::set("SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH", "0");
+
+    sdl_context.mouse().show_cursor(false);
+
+    let window_flags = SDL_WindowFlags::SDL_WINDOW_ALWAYS_ON_TOP as u32
+        | SDL_WindowFlags::SDL_WINDOW_SKIP_TASKBAR as u32
+        | SDL_WindowFlags::SDL_WINDOW_BORDERLESS as u32;
+
+    let mut window = video_subsystem
+        .window("", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .set_window_flags(window_flags)
+        .borderless()
+        .opengl()
+        .allow_highdpi()
+        .build()
+        .unwrap();
+
+    let ctx = window.gl_create_context().unwrap();
+
+    // SDL_GetWindowWMInfo(&mut window, info)
 
     // Window Transparency and disable Borders
-    glfw.window_hint(glfw::WindowHint::TransparentFramebuffer(true));
-    glfw.window_hint(glfw::WindowHint::Decorated(false));
+    // glfw.window_hint(glfw::WindowHint::TransparentFramebuffer(true));
+    // glfw.window_hint(glfw::WindowHint::Decorated(false));
 
     // This puts our windows always on top
-    glfw.window_hint(glfw::WindowHint::Floating(true));
-    glfw.window_hint(glfw::WindowHint::Focused(false));
-    glfw.window_hint(glfw::WindowHint::FocusOnShow(false));
-    glfw.window_hint(glfw::WindowHint::CenterCursor(false));
-    glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::OpenGl));
+    // glfw.window_hint(glfw::WindowHint::Floating(true));
+    // glfw.window_hint(glfw::WindowHint::Focused(false));
+    // glfw.window_hint(glfw::WindowHint::FocusOnShow(false));
+    // glfw.window_hint(glfw::WindowHint::CenterCursor(false));
+    // glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::OpenGl));
 
-    let (mut window, events) = glfw
-        .create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW Window");
+    // let (mut window, events) = glfw
+    //     .create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "", glfw::WindowMode::Windowed)
+    //     .expect("Failed to create GLFW Window");
 
-    window.make_current();
-    window.set_key_polling(true);
-    window.set_scroll_polling(true);
-    window.set_framebuffer_size_polling(true);
+    // window.make_current();
+    // window.set_key_polling(true);
+    // window.set_scroll_polling(true);
+    // window.set_framebuffer_size_polling(true);
 
-    // window.set_cursor_mode(glfw::CursorMode::Disabled);
-    window.set_cursor_enter_polling(true);
-    window.set_focus_polling(true);
-    window.set_mouse_button_polling(false);
+    // // window.set_cursor_mode(glfw::CursorMode::Disabled);
+    // window.set_cursor_enter_polling(true);
+    // window.set_focus_polling(true);
+    // window.set_mouse_button_polling(false);
 
-    gl::load_with(|name| window.get_proc_address(name) as *const _);
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
 
-    let monitor = glfw::Monitor::from_primary();
-    let mode = glfw::Monitor::get_video_mode(&monitor).unwrap();
+    // let monitor = glfw::Monitor::from_primary();
+    // let mode = glfw::Monitor::get_video_mode(&monitor).unwrap();
 
-    let win_center_x = (mode.width / 2) - (WINDOW_WIDTH / 2);
-    let win_center_y = (mode.height / 2) - (WINDOW_HEIGHT / 2);
+    // let win_center_x = (mode.width / 2) - (WINDOW_WIDTH / 2);
+    // let win_center_y = (mode.height / 2) - (WINDOW_HEIGHT / 2);
 
-    window.set_pos((win_center_x) as i32, (win_center_y) as i32);
+    // window.set_pos((win_center_x) as i32, (win_center_y) as i32);
 
     // OpenGL set up
     let default_shader = engine::shader::create_shader_from_files(
@@ -116,12 +158,16 @@ fn main() {
         gl::Uniform1i(default_shader.get_uniform_loc_from_name("ourTexture"), 0);
     }
 
-    while !window.should_close() {
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut is_running = true;
+
+    while is_running {
         // OpenGL set ups
         unsafe {
             gl::Viewport(0, 0, WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32);
 
-            // gl::ClearColor(0.1, 0.3, 0.5, 1.0);
+            // gl::ClearColor(1.0, 1.0, 1.0, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
             gl::Clear(gl::DEPTH_TEST);
             gl::Enable(gl::DEPTH_TEST);
@@ -130,8 +176,17 @@ fn main() {
         }
 
         // user inputs
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. }
+                | sdl2::event::Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    is_running = false;
+                }
+                _ => {}
+            }
         }
 
         // Changes and Updates
@@ -151,8 +206,7 @@ fn main() {
             );
         }
 
-        window.swap_buffers();
-        glfw.poll_events();
+        window.gl_swap_window();
     }
 
     default_buffer.clean();
@@ -160,19 +214,19 @@ fn main() {
     default_shader.delete_shader();
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true);
-        }
-        glfw::WindowEvent::CursorEnter(_) => {
-            println!("nois que voa");
-            window.set_cursor_pos((WINDOW_WIDTH + 40) as f64, WINDOW_HEIGHT as f64);
-        }
-        glfw::WindowEvent::Focus(_) => {
-            println!("here {:?}", window.is_framebuffer_transparent());
-            window.set_cursor_pos((WINDOW_WIDTH + 40) as f64, WINDOW_HEIGHT as f64);
-        }
-        _ => {}
-    }
-}
+// fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+//     match event {
+//         glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+//             window.set_should_close(true);
+//         }
+//         glfw::WindowEvent::CursorEnter(_) => {
+//             println!("nois que voa");
+//             window.set_cursor_pos((WINDOW_WIDTH + 40) as f64, WINDOW_HEIGHT as f64);
+//         }
+//         glfw::WindowEvent::Focus(_) => {
+//             println!("here {:?}", window.is_framebuffer_transparent());
+//             window.set_cursor_pos((WINDOW_WIDTH + 40) as f64, WINDOW_HEIGHT as f64);
+//         }
+//         _ => {}
+//     }
+// }
